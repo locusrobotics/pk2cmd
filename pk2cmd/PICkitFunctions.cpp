136d3ee669635ddd30b02963690fdcfdf0b38c3e
Tom Nurse <tnurse@locusrobotics.com>
2025-06-24 11:15:41 -0400

    Fix formatting

--- pk2cmd/PICkitFunctions.cpp
+++ pk2cmd/PICkitFunctions.cpp
@@ -2982,12 +2982,12 @@ void CPICkitFunctions::timerPrint(void)
 	if (usePercentTimer)
 	{
 		timerValue += timerIncrement;
-
-        // Ensure percentage cannot exceed 100%
-        if (timerValue > 100)
-        {
-            timerValue = 100;
-        }
+        
+		// Ensure percentage cannot exceed 100%
+		if (timerValue > 100)
+		{
+			timerValue = 100;
+		}
 
 		if ((int)lastTimerValue != (int)timerValue)
 		{
